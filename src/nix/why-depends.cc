onixcomputer/nix

commit 1dbf2d5615fbf601770da8880f19e12f2193d05c

Prevent uninitialized StorePath creation

src/nix/why-depends.cc
@@ -103,7 +103,7 @@ struct CmdWhyDepends : SourceExprCommand
         std::map<StorePath, Node> graph;
 
         for (auto & path : closure)
-            graph.emplace(path.clone(), Node{path.clone(), cloneStorePathSet(store->queryPathInfo(path)->references)});
+            graph.emplace(path.clone(), Node { .path = path.clone(), .refs = cloneStorePathSet(store->queryPathInfo(path)->references) });
 
         // Transpose the graph.
         for (auto & node : graph)
@@ -112,7 +112,7 @@ struct CmdWhyDepends : SourceExprCommand
 
         /* Run Dijkstra's shortest path algorithm to get the distance
            of every path in the closure to 'dependency'. */
-        graph[dependencyPath.clone()].dist = 0;
+        graph.emplace(dependencyPath.clone(), Node { .path = dependencyPath.clone(), .dist = 0 });
 
         std::priority_queue<Node *> queue;
 

src/libstore/path.hh
@@ -18,6 +18,8 @@ extern "C" {
 
 struct StorePath : rust::Value<3 * sizeof(void *) + 24, ffi_StorePath_drop>
 {
+    StorePath() = delete;
+
     static StorePath make(std::string_view path, std::string_view storeDir);
 
     static StorePath make(unsigned char hash[20], std::string_view name);

src/libutil/rust-ffi.hh
@@ -113,6 +113,8 @@ extern "C" {
 
 struct String : Vec<char, ffi_String_drop>
 {
+    String() = delete;
+
     String(std::string_view s)
     {
         ffi_String_new(StringSlice(s), this);

commit 7b5fe29900064f97754f8ea0c1cca097fb9d6ada

nix why-depends: Fix shortest path calculation
This was completely broken since 1dbf2d5615fbf601770da8880f19e12f2193d05c.

src/nix/why-depends.cc
@@ -106,7 +106,11 @@ struct CmdWhyDepends : SourceExprCommand
         std::map<StorePath, Node> graph;
 
         for (auto & path : closure)
-            graph.emplace(path, Node { .path = path, .refs = store->queryPathInfo(path)->references });
+            graph.emplace(path, Node {
+                .path = path,
+                .refs = store->queryPathInfo(path)->references,
+                .dist = path == dependencyPath ? 0 : inf
+            });
 
         // Transpose the graph.
         for (auto & node : graph)
@@ -115,8 +119,6 @@ struct CmdWhyDepends : SourceExprCommand
 
         /* Run Dijkstra's shortest path algorithm to get the distance
            of every path in the closure to 'dependency'. */
-        graph.emplace(dependencyPath, Node { .path = dependencyPath, .dist = 0 });
-
         std::priority_queue<Node *> queue;
 
         queue.push(&graph.at(dependencyPath));