7df226ef2aedf9f2276efc78d2df36e75668e064
brittonr <b@robitzs.ch>
2026-03-16 15:43:32 -0400

    Fix wasm.cc compilation against 2.33.3 API
    - Add local wasmRealisePath() since realisePath is static in primops.cc
      and not a method on EvalState in 2.33.3 (it became one later on master)
    - Use .fun instead of .impl for PrimOp field (2.33.3 naming)
    - Explicit std::vector for wasiConfig.argv() to fix brace-init
    - Split getExport<Func>().call() to avoid template parse issue
    - Clear lowdown patches that don't apply when nixpkgs follows brings 2.0.4 patches to 2.0.2

--- packaging/dependencies.nix
+++ packaging/dependencies.nix
@@ -52,6 +52,20 @@ scope: {
     zlibSupport = true;
   };
 
+  lowdown = pkgs.lowdown.overrideAttrs (prevAttrs: rec {
+    version = "2.0.2";
+    src = pkgs.fetchurl {
+      url = "https://kristaps.bsd.lv/lowdown/snapshots/lowdown-${version}.tar.gz";
+      hash = "sha512-cfzhuF4EnGmLJf5EGSIbWqJItY3npbRSALm+GarZ7SMU7Hr1xw0gtBFMpOdi5PBar4TgtvbnG4oRPh+COINGlA==";
+    };
+    # Clear patches from newer nixpkgs lowdown that don't apply to 2.0.2
+    patches = [ ];
+    nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.buildPackages.bmake ];
+    postInstall =
+      lib.replaceStrings [ "lowdown.so.1" "lowdown.1.dylib" ] [ "lowdown.so.2" "lowdown.2.dylib" ]
+        (prevAttrs.postInstall or "");
+  });
+
   libblake3 = pkgs.libblake3.override {
     useTBB =
       !(
